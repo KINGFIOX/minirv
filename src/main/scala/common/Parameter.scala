a6c2832bc31f7a7b30d324a0420e612da98a790f
wangfiox <wangfiox@gmail.com>
2024-07-11 23:33:52 +0800

    :art:

--- src/main/scala/common/Parameter.scala
+++ src/main/scala/common/Parameter.scala
@@ -4,11 +4,18 @@ import chisel3._
 import chisel3.util._
 
 /** @brief
+  *   有几个通用寄存器
+  */
+trait HasRegFileParameter {
+  val NRReg = 32
+}
+
+/** @brief
   *   有 core 的一些参数
   */
 trait HasCoreParameter {
-  val XLEN      = 32 // 机器字长
-  val AddrBits  = XLEN // AddrBits is used in some cases
-  val DataBits  = XLEN // 一个 word 有几个 bit
-  val DataBytes = DataBits >> 3 // 一个 word 有几个字节
+  val XLEN = 32 // 机器字长
+  // val AddrBits  = XLEN // AddrBits is used in some cases
+  // val DataBits  = XLEN // 一个 word 有几个 bit
+  val DataBytes = XLEN >> 3 // 一个 word 有几个字节
 }
