66a93975ae90b7df44757ba190ccb6a94ff3217b
wangfiox <wangfiox@gmail.com>
2024-07-11 23:33:26 +0800

    改了ALU结构

--- src/main/scala/component/ALU.scala
+++ src/main/scala/component/ALU.scala
@@ -6,21 +6,26 @@ import common.HasCoreParameter
 import upickle.default
 
 object ALUOpType extends ChiselEnum {
-  val alu_ADD, alu_SUB, alu_AND, alu_OR, alu_XOR, alu_SLL, alu_SRL, alu_SRA, alu_LT, alu_LTU, alu_GE, alu_GEU, alu_EQ, alu_NE = Value
+  val alu_X, alu_ADD, alu_SUB, alu_AND, alu_OR, alu_XOR, alu_SLL, alu_SRL, alu_SRA, alu_SLT, alu_SLTU, alu_BLT, alu_BLTU, alu_BGE, alu_BGEU, alu_BEQ, alu_BNE, alu_JALR = Value
 }
 
 class ALU extends Module with HasCoreParameter {
   val io = IO(new Bundle {
-    val a      = Input(UInt(XLEN.W))
-    val b      = Input(UInt(XLEN.W))
-    val alu_op = Input(ALUOpType())
-    val out    = Output(UInt(XLEN.W))
+    val a       = Input(UInt(XLEN.W))
+    val b       = Input(UInt(XLEN.W)) // 这个可能是: 立即数, 寄存器值, 或者是 PC
+    val alu_op  = Input(ALUOpType())
+    val out     = Output(UInt(XLEN.W))
+    val br_flag = Output(Bool())
   })
 
-  io.out := 0.U // default
+  io.out     := 0.U // default
+  io.br_flag := false.B // 默认不跳转
 
   // 定义 ALU 的操作
   switch(io.alu_op) {
+    is(ALUOpType.alu_X) {
+      io.out := 0.U // ALU 啥也不干
+    }
     is(ALUOpType.alu_ADD) {
       io.out := io.a + io.b
     }
@@ -45,23 +50,44 @@ class ALU extends Module with HasCoreParameter {
     is(ALUOpType.alu_SRA) {
       io.out := (io.a.asSInt >> io.b(log2Up(XLEN) - 1, 0)).asUInt
     }
-    is(ALUOpType.alu_LT) {
+    is(ALUOpType.alu_SLT) {
       io.out := (io.a.asSInt < io.b.asSInt).asUInt
     }
-    is(ALUOpType.alu_LTU) {
+    is(ALUOpType.alu_SLTU) {
       io.out := (io.a < io.b).asUInt
     }
-    is(ALUOpType.alu_GE) {
-      io.out := (io.a.asSInt >= io.b.asSInt).asUInt
+    is(ALUOpType.alu_BLT) {
+      when(io.a.asSInt < io.b.asSInt) {
+        io.br_flag := true.B
+      }
     }
-    is(ALUOpType.alu_GEU) {
-      io.out := (io.a >= io.b).asUInt
+    is(ALUOpType.alu_BLTU) {
+      when(io.a < io.b) {
+        io.br_flag := true.B
+      }
     }
-    is(ALUOpType.alu_EQ) {
-      io.out := (io.a === io.b).asUInt
+    is(ALUOpType.alu_BGE) {
+      when(io.a.asSInt >= io.b.asSInt) {
+        io.br_flag := true.B
+      }
     }
-    is(ALUOpType.alu_NE) {
-      io.out := (io.a =/= io.b).asUInt
+    is(ALUOpType.alu_BGEU) {
+      when(io.a >= io.b) {
+        io.br_flag := true.B
+      }
+    }
+    is(ALUOpType.alu_BEQ) {
+      when(io.a === io.b) {
+        io.br_flag := true.B
+      }
+    }
+    is(ALUOpType.alu_BNE) {
+      when(io.a =/= io.b) {
+        io.br_flag := true.B
+      }
+    }
+    is(ALUOpType.alu_JALR) {
+      io.out := (io.a + io.b) & ~1.U(XLEN.W)
     }
   }
 }
