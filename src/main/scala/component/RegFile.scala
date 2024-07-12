baadd6bc70580d28961a5ae8a4c6978d10a452cc
wangfiox <wangfiox@gmail.com>
2024-07-12 16:03:28 +0800

    :art:

--- src/main/scala/component/RegFile.scala
+++ src/main/scala/component/RegFile.scala
@@ -5,10 +5,14 @@ import chisel3.util._
 import common.HasCoreParameter
 import common.HasRegFileParameter
 
+object WB_sel extends ChiselEnum {
+  val wbsel_X, wbsel_ALU, wbsel_CSR, wbsel_MEM, wbsel_PC4 /* jal & jalr */ = Value
+}
+
 /** @brief
   *   寄存器堆, 但是其实不是一个 chisel 的 Module
   */
-class RegFile extends HasRegFileParameter with HasCoreParameter {
+private class RegFile extends HasRegFileParameter with HasCoreParameter {
   val rf                            = Mem(NRReg, UInt(XLEN.W))
   def read(addr: UInt): UInt        = Mux(addr === 0.U, 0.U, rf(addr))
   def write(addr: UInt, data: UInt) = { rf(addr) := Mux(addr === 0.U, 0.U, data(XLEN - 1, 0)) }
