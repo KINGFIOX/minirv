3e33178d3bba45ba54cb8e0a8e5c76b51d87e6a5
wangfiox <wangfiox@gmail.com>
2024-07-14 23:18:40 +0800

    添加blackbox -> ip 核

---
+++ src/main/scala/io/blackbox/DistributedSinglePortROM.scala
@@ -0,0 +1,11 @@
+package io.blackbox
+
+import chisel3._
+import chisel3.util._
+
+class DistributedSinglePortROM(depth: Int, data_width: Int) extends BlackBox {
+  val io = IO(new Bundle {
+    val a   = Input(UInt(log2Ceil(depth).W)) // addr
+    val spo = Output(UInt(data_width.W)) // data
+  })
+}
---
+++ src/main/scala/io/blackbox/PLL.scala
@@ -0,0 +1,33 @@
+package io.blackbox
+
+import chisel3._
+import chisel3.util._
+
+/** @brief
+  *   时钟降频转换
+  */
+class PLL extends BlackBox {
+  val io = IO(new Bundle {
+    val clk_in1  = Input(Clock())
+    val clk_out1 = Output(Clock())
+    val locked   = Output(Bool())
+  })
+}
+
+object DigDecoder extends App {
+
+  /** @brief
+    *   这里验证一下: 会不会有 io_ 前缀。 实验证明: 并没有
+    */
+  val s = _root_.circt.stage.ChiselStage.emitSystemVerilogFile(
+    new Module {
+      val pll = Module(new PLL)
+      pll.io <> DontCare
+    },
+    args = Array("--target-dir", "generated"),
+    firtoolOpts = Array(
+      "--strip-debug-info",
+      "-disable-all-randomization"
+    )
+  )
+}
