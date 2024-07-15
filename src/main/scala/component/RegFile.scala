725d8eaeeb08983faefa4dab00647b07ce964acb
wangfiox <wangfiox@gmail.com>
2024-07-15 15:29:21 +0800

    :art:

--- src/main/scala/component/RegFile.scala
+++ src/main/scala/component/RegFile.scala
@@ -13,7 +13,7 @@ object WB_sel extends ChiselEnum {
   *   寄存器堆, 但是其实不是一个 chisel 的 Module
   */
 private class RegFile extends HasRegFileParameter with HasCoreParameter {
-  val rf                            = Mem(NRReg, UInt(XLEN.W))
-  def read(addr: UInt): UInt        = Mux(addr === 0.U, 0.U, rf(addr))
-  def write(addr: UInt, data: UInt) = { rf(addr) := Mux(addr === 0.U, 0.U, data(XLEN - 1, 0)) }
+  private val _rf                   = Mem(NRReg, UInt(XLEN.W))
+  def read(addr: UInt): UInt        = Mux(addr === 0.U, 0.U, _rf(addr))
+  def write(addr: UInt, data: UInt) = { _rf(addr) := Mux(addr === 0.U, 0.U, data(XLEN - 1, 0)) }
 }
