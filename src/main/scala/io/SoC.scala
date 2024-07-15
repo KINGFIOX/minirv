8e10a9509d924ecabb881908909644c746a2614e
wangfiox <wangfiox@gmail.com>
2024-07-15 14:12:18 +0800

    copy of 萝老

---
+++ src/main/scala/io/SoC.scala
@@ -0,0 +1,43 @@
+package io
+
+import chisel3._
+import chisel3.util._
+import io.blackbox.DistributedSinglePortRAM
+import io.blackbox.DistributedSinglePortROM
+import common.HasCoreParameter
+import io.device.HasSevenSegParameter
+import io.blackbox.PLL
+import io.blackbox.CLKGen
+
+trait HasSocParameter {
+  val addrBits = 14
+  def iromLens = 1 << 14 // rom 有 (1 << 14) 个 word
+  def dramLens = 1 << 14
+
+  val switchBits = 24
+  val buttonBits = 5
+
+  val ledBits = 24 // 因为有 24 个  led 灯
+}
+
+class SoC extends Module with HasSevenSegParameter with HasSocParameter with HasCoreParameter {
+  val io = IO(new Bundle {
+    val switch = Input(UInt(switchBits.W)) // 拨码开关
+    val button = Input(UInt(buttonBits.W)) //
+    // 8 个数码管
+    val dig_en = Output(UInt(digitNum.W))
+    val DN_A   = Output(Bool())
+    val DN_B   = Output(Bool())
+    val DN_C   = Output(Bool())
+    val DN_D   = Output(Bool())
+    val DN_E   = Output(Bool())
+    val DN_F   = Output(Bool())
+    val DN_G   = Output(Bool())
+    val DN_DP  = Output(Bool())
+    val led    = Output(UInt(ledBits.W))
+  })
+
+  private val cpu_clock = CLKGen(this.clock)
+
+  withClock(cpu_clock) {}
+}
