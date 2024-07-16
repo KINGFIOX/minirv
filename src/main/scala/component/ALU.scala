2fe9aa392b8ed5e8cd19c3a601ae5c62fa63183b
wangfiox <wangfiox@gmail.com>
2024-07-16 14:32:06 +0800

    _v 表示 value, _i 表示 index

--- src/main/scala/component/ALU.scala
+++ src/main/scala/component/ALU.scala
@@ -24,8 +24,8 @@ object ALUOpType extends ChiselEnum {
 
 class ALU extends Module with HasCoreParameter {
   val io = IO(new Bundle {
-    val op1    = Input(UInt(XLEN.W))
-    val op2    = Input(UInt(XLEN.W)) // 这个可能是: 立即数, 寄存器值
+    val op1_v  = Input(UInt(XLEN.W))
+    val op2_v  = Input(UInt(XLEN.W)) // 这个可能是: 立即数, 寄存器值
     val alu_op = Input(ALUOpType())
     val out    = Output(UInt(XLEN.W))
   })
@@ -38,39 +38,39 @@ class ALU extends Module with HasCoreParameter {
       io.out := 0.U // ALU 啥也不干
     }
     is(ALUOpType.alu_ADD) {
-      io.out := (io.op1.asSInt + io.op2.asSInt).asUInt
+      io.out := (io.op1_v.asSInt + io.op2_v.asSInt).asUInt
     }
     is(ALUOpType.alu_SUB) {
-      io.out := (io.op1.asSInt - io.op2.asSInt).asUInt
+      io.out := (io.op1_v.asSInt - io.op2_v.asSInt).asUInt
     }
     is(ALUOpType.alu_AND) {
-      io.out := io.op1 & io.op2
+      io.out := io.op1_v & io.op2_v
     }
     is(ALUOpType.alu_OR) {
-      io.out := io.op1 | io.op2
+      io.out := io.op1_v | io.op2_v
     }
     is(ALUOpType.alu_XOR) {
-      io.out := io.op1 ^ io.op2
+      io.out := io.op1_v ^ io.op2_v
     }
     is(ALUOpType.alu_SLL) {
-      io.out := io.op1 << io.op2(log2Up(XLEN) - 1, 0) // 假设最多移位 31 位
+      io.out := io.op1_v << io.op2_v(log2Up(XLEN) - 1, 0) // 假设最多移位 31 位
     }
     is(ALUOpType.alu_SRL) {
-      io.out := io.op1 >> io.op2(log2Up(XLEN) - 1, 0)
+      io.out := io.op1_v >> io.op2_v(log2Up(XLEN) - 1, 0)
     }
     is(ALUOpType.alu_SRA) {
-      io.out := (io.op1.asSInt >> io.op2(log2Up(XLEN) - 1, 0)).asUInt
+      io.out := (io.op1_v.asSInt >> io.op2_v(log2Up(XLEN) - 1, 0)).asUInt
     }
     is(ALUOpType.alu_SLT) {
-      io.out := (io.op1.asSInt < io.op2.asSInt).asUInt
+      io.out := (io.op1_v.asSInt < io.op2_v.asSInt).asUInt
     }
     is(ALUOpType.alu_SLTU) {
-      io.out := io.op1 < io.op2
+      io.out := io.op1_v < io.op2_v
     }
     is(ALUOpType.alu_JALR) {
       // rs1
       // rs2 = sext(offset)
-      io.out := (io.op1 + io.op2) & ~1.U(XLEN.W)
+      io.out := (io.op1_v + io.op2_v) & ~1.U(XLEN.W)
     }
   }
 }
