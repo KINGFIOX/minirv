47bdf369962b054cde3fa9c1e91b54306a9599fb
wangfiox <wangfiox@gmail.com>
2024-07-16 19:06:58 +0800

    添加可配置选项

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,10 @@
+object ENV {
+  // 定义: 是输出到 vivado, 还是 verilator
+}
+
+/** @brief
+  *   .sv 文件要输出到 cdp-tests/mySoC/miniRV_SoC.v
+  */
 object Hello {
   def main(args: Array[String]) = {
     val x = 12
