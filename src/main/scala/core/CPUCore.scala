ec1a30d15b980881b8430c7e75cbb25a681f73ef
wangfiox <wangfiox@gmail.com>
2024-07-17 17:28:24 +0800

    因为的时序与golden model正好是差了一个周期, 因为我直接来一个 RegNext 延迟一个周期, 有种土鳖中的战斗鳖的感觉

--- src/main/scala/core/CPUCore.scala
+++ src/main/scala/core/CPUCore.scala
@@ -112,23 +112,26 @@ class CPUCore extends Module with HasCoreParameter {
 
   /* ---------- debug ---------- */
 
-  io.debug.wb_have_inst := true.B
-  io.debug.wb_pc        := (if_.io.out.pc_4 - 4.U)
+  // 这个 debug 的线，正好差了一个周期，懒得整了，直接 RegNext
+
+  io.debug.wb_have_inst := RegNext(true.B)
+  io.debug.wb_pc        := RegNext(if_.io.out.pc_4 - 4.U)
   printf("wb_pc=%x\n", io.debug.wb_pc)
   printf("pc_4=%x\n", if_.io.out.pc_4)
-  io.debug.wb_ena := Mux(cu_.io.ctrl.wb_sel =/= WB_sel.wbsel_X, true.B, false.B)
-  io.debug.wb_reg := cu_.io.rf.rd_i
+  io.debug.wb_ena := RegNext(Mux(cu_.io.ctrl.wb_sel =/= WB_sel.wbsel_X, true.B, false.B))
+  io.debug.wb_reg := RegNext(cu_.io.rf.rd_i)
   printf("wb_reg=%x\n", io.debug.wb_reg)
   printf("wb_value=%x\n", io.debug.wb_value)
-  io.debug.wb_value := MuxCase(
-    0.U,
-    Seq(
-      (cu_.io.ctrl.wb_sel === WB_sel.wbsel_ALU) -> alu_.io.out,
-      (cu_.io.ctrl.wb_sel === WB_sel.wbsel_MEM) -> mem_.io.out.rdata,
-      (cu_.io.ctrl.wb_sel === WB_sel.wbsel_PC4) -> if_.io.out.pc_4
+  io.debug.wb_value := RegNext(
+    MuxCase(
+      0.U,
+      Seq(
+        (cu_.io.ctrl.wb_sel === WB_sel.wbsel_ALU) -> alu_.io.out,
+        (cu_.io.ctrl.wb_sel === WB_sel.wbsel_MEM) -> mem_.io.out.rdata,
+        (cu_.io.ctrl.wb_sel === WB_sel.wbsel_PC4) -> if_.io.out.pc_4
+      )
     )
   )
-
   // val rd = cu_.io.rf.rd_i
 }
 
