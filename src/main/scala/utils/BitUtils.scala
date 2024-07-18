8c4c569710610a8abd399d2b616acbe731b26e52
wangfiox <wangfiox@gmail.com>
2024-07-18 14:52:42 +0800

    scala 的 int 地址转换为 UINT

--- src/main/scala/utils/BitUtils.scala
+++ src/main/scala/utils/BitUtils.scala
@@ -5,9 +5,12 @@ import chisel3.util._
 import common.HasCoreParameter
 
 object ZeroExt extends HasCoreParameter {
-  def apply(a: UInt)(implicit XLEN: Int) = {
+  def apply(a: UInt)(implicit XLEN: Int): UInt = {
     val aLen = a.getWidth
     if (aLen >= XLEN) a(XLEN - 1, 0) else Cat(0.U((XLEN - aLen).W), a)
+  }
+  def apply(addr: Int)(implicit XLEN: Int): UInt = {
+    BigInt(addr & 0xffff_ffffL).asUInt
   }
 }
 
