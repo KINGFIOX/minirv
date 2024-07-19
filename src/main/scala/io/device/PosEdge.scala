750ca42125b4ffeb0f5d4d13313df39679298d7c
wangfiox <wangfiox@gmail.com>
2024-07-19 16:32:55 +0800

    上升沿检测

---
+++ src/main/scala/io/device/PosEdge.scala
@@ -0,0 +1,13 @@
+package io.device
+
+import chisel3._
+import chisel3.util._
+
+object PosEdge {
+  def apply(sig: Bool): Bool = {
+    sig & ~RegNext(sig)
+  }
+  def apply(sig: UInt): UInt = {
+    sig & ~RegNext(sig)
+  }
+}
