1e702f0675e6b84521dd05c5cb82718c472958d5
wangfiox <wangfiox@gmail.com>
2024-07-21 23:41:47 +0800

    :tada: 测试

--- src/test/scala/CUSpec.scala
+++ src/test/scala/CUSpec.scala
@@ -4,29 +4,41 @@ import chisel3.simulator.EphemeralSimulator._
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.must.Matchers
 import hitsz.common.Instructions
+import hitsz.io.miniRV_SoC
+
+import chisel3.testers.BasicTester
+
+import _root_.rv32i.RVEMU
+import rv32i.Settings
+import rv32i.Main.readFile
 
 class GCDSpec extends AnyFreeSpec with Matchers {
 
+  val user: Array[Byte] = readFile("meminit.bin") match {
+    case Right(bytes) => bytes
+    case Left(error) => Array[Byte]()
+  }
+
+  val kernel = readFile("trap_handle.bin") match {
+    case Right(bytes) => bytes
+    case Left(error) => Array[Byte]()
+  }
+
+  val emu = new RVEMU(user, kernel)
+
   "test BitPat" in {
-    simulate(new Module {
+    simulate(new miniRV_SoC) { dut =>
+      // 初始化
+      dut.io.fpga_rst.poke(true.B)
+      dut.io.fpga_clk.step(10)
+      dut.io.fpga_rst.poke(false.B)
 
-      val io = IO(new Bundle {
-        val inst = Input(UInt(32.W))
-        val out  = Output(Bool())
-      })
-
-      io.out := false.B
-
-      when(io.inst === Instructions.ADD) {
-        io.out := true.B
-        printf("ADD\n")
+      for (_ <- 0 until 10) {
+        val dbg = dut.io.dbg
+        emu.step()
+        dut.io.fpga_clk.step()
+        println(s"${dbg}")
       }
-
-    }) { dut =>
-      val inst = "b00000001111111111000111110110011".U;
-      dut.io.inst.poke(inst)
-      dut.clock.step()
-      dut.io.out.expect(true.B)
     }
   }
 }
